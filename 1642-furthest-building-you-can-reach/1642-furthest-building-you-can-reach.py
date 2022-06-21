b15cf6b167fbdefbfa2d7007047d718c0e8eb6d2
bogoconic1 <100673850+bogoconic1@users.noreply.github.com>
2022-06-21 09:19:39 +0800

    Time: 1132 ms (17.66%), Space: 28.5 MB (89.32%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,33 +1,17 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         
-        def check(mid):
+        q = []
+        
+        for i,(x,y) in enumerate(zip(heights,heights[1:])):
             
-            brem, lrem = bricks, ladders
-
-            canTake = heights[:mid+1]
-            diff = sorted([y-x for x,y in zip(canTake,canTake[1:])])
-            for x in diff:
-                if x <= 0:
-                    continue
-                elif brem >= x:
-                    brem -= x
-                else:
-                    if lrem == 0:
-                        return False
-                    lrem -= 1
-                    
-            return True
-        
-        
-        l,r = 0, len(heights)-1
-        while l < r:
-            mid = l+r >> 1
-            if check(mid):
-                l = mid+1
-            else:
-                r = mid
+            d = y-x
+            if d > 0:
+                heappush(q,d)
                 
-        if check(l):
-            return l
-        return l-1
+            if len(q) > ladders:
+                bricks -= heappop(q)
+                if bricks < 0:
+                    return i
+                
+        return len(heights)-1
