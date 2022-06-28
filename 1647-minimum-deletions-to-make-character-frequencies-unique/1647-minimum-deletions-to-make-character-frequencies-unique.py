b0535772f18fdb31c4fea5075f1e716344f8426a
bogoconic1 <100673850+bogoconic1@users.noreply.github.com>
2022-06-28 09:02:16 +0800

    Time: 192 ms (69.70%), Space: 14.8 MB (51.90%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(len(freq)):
             if freq[i] >= prev:
                 ans += min(freq[i],freq[i]-prev+1)
-                freq[i] = prev-1
+                freq[i] = max(0,prev-1)
             prev = freq[i]
             
         #print(freq)
