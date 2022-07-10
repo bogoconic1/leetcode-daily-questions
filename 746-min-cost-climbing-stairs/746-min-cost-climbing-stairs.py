aa9ef4e8874a3ce142c72d2ae79e5e7e67fafbe0
bogoconic1 <100673850+bogoconic1@users.noreply.github.com>
2022-07-10 10:20:44 +0800

    Time: 108 ms (27.24%), Space: 14 MB (44.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,12 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, nums: List[int]) -> int:
         
-        @cache
-        def dp(i):
-            
-            if i >= len(nums):
-                return 0
-            
-            return nums[i] + min(dp(i+1),dp(i+2))
+        n = len(nums)
         
-        return min(dp(0),dp(1))
+        dp = [float("inf")]*(n+1)
+        dp[0] = 0
+        dp[1] = 0
+        
+        for i in range(1,n+1):
+            if i == 1:
+                dp[i] = min(dp[i], nums[i-1] + dp[i-1])
+            else:
+                dp[i] = min(dp[i], nums[i-1] + dp[i-1], nums[i-2] + dp[i-2])
+                
+                
+        return dp[n]
