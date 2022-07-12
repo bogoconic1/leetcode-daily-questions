6a7786a0b5f90199571697d8ac4df55bc74ab0e4
bogoconic1 <100673850+bogoconic1@users.noreply.github.com>
2022-07-12 21:50:16 +0800

    Time: 375 ms (97.79%), Space: 20.2 MB (92.86%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestDivisor(self, piles: List[int], h: int) -> int:
+        
+        def feasible(speed) -> bool:
+            # return sum(math.ceil(pile / speed) for pile in piles) <= H  # slower        
+            return sum(ceil(pile/speed) for pile in piles) <= h  # faster
+
+        left, right = 1, max(piles)
+        while left < right:
+            mid = left  + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+        
