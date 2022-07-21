d97b983d77080b489451de9cf8fa340cc30954f2
bogoconic1 <100673850+bogoconic1@users.noreply.github.com>
2022-07-21 12:45:23 +0800

    Time: 53 ms (38.31%), Space: 14.1 MB (18.38%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -7,46 +7,49 @@ class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         
         def reverse(move):
-            nonlocal head
             prev,cur = None,move
-            i = 0
             
             while cur:
                 tmp = cur.next
                 cur.next = prev
                 prev = cur
                 cur = tmp
-                if i == 0:
-                    prev.next = head
-                i += 1
                 
             return prev
         
-        to_rev_ptr = ListNode()
-        to_rev = to_rev_ptr
-        dummy = ListNode()
-        cur = ListNode()
-        prev = ListNode()
-        prev.next = head
-        cur = prev
-        
-        index = 1
-        while head:
-            if left <= index <= right:
-                to_rev.next = head
-                to_rev = to_rev.next
-                #head.next = reverse(head)
-            head = head.next
-            if index == right:
-                break
-            index += 1
+        def merge(a,b,c):
+            dummy = tail = ListNode()
+            while a:
+                tail.next = a
+                tail = tail.next
+                a = a.next
+            while b:
+                tail.next = b
+                tail = tail.next
+                b = b.next
+            while c:
+                tail.next = c
+                tail = tail.next
+                c = c.next
             
-        to_rev.next = None
-        to_rev_ptr = reverse(to_rev_ptr.next)
+            tail.next = None
+            return dummy.next
         
-        for _ in range(left-1):
-            cur = cur.next
+        def split(head,threshold):
+            if threshold == 1:
+                return None,head
+            slow = ListNode(-1000)
+            slow.next = head
+            index = 1
+            while index < threshold:
+                slow = slow.next
+                index += 1
+            secondhead = slow.next
+            slow.next = None
+            return head,secondhead
 
-        cur.next = to_rev_ptr
-        dummy.next = prev.next
-        return dummy.next
+        a,b = split(head,left)
+        b,c = split(b,right-left+2)
+        b = reverse(b)
+        #return b
+        return merge(a,b,c)
